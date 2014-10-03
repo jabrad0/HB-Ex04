21e1e6ebb814b296201d30da75e7f48f73a3d60b
Hackbright Student <info@hackbrightacademy.com>
2014-10-03 12:08:15 -0700

    changes made after code review

--- list_operations.py
+++ list_operations.py
@@ -71,8 +71,7 @@ def replace_head(input_list):
 
 def replace_third_and_last(input_list):
     """Replace the third and last elements of the input list with the value 37."""
-    input_list[2] = 37
-    input_list[-1] = 37
+    input_list[2], input_list[-1] = 37,37
     return input_list 
 
 def replace_middle(input_list):
@@ -92,8 +91,7 @@ def replace_middle(input_list):
 
 def delete_third_and_seventh(input_list):
     """Remove the third and seventh elements of the input list."""
-    del input_list[2]
-    del input_list[6]
+    del input_list[6], input_list[2]
     return input_list 
 
 def delete_middle(input_list):
